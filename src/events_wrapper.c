abb5ff801965fc215db6fe3c42a5ba0c0d8ec584
Antoine Bache <antoine.bache@epitech.eu>
2017-08-07 17:47:02 +0200

    Raycasting in asm now

--- src/events_wrapper.c
+++ src/events_wrapper.c
@@ -2,9 +2,6 @@
 #include <assert.h>
 #include <stdint.h>
 
-#define MAP_WIDTH 640
-#define MAP_HEIGHT 480
-
 // ASM Bindings
 void wolfasm_player_move_forward(void) __asm__("wolfasm_player_move_forward");
 void wolfasm_player_move_backward(void) __asm__("wolfasm_player_move_backward");
@@ -26,7 +23,7 @@ void wolfasm_events_mouse_down_cwrapper(SDL_Event const *events) __asm__(
 void wolfasm_events_mouse_motion_cwrapper(SDL_Event const *events) __asm__(
     "wolfasm_events_mouse_motion_cwrapper");
 
-void wolfasm_raycast_pix_crwapper(void) __asm__(
+void wolfasm_raycast_pix_crwapper(int32_t x) __asm__(
     "wolfasm_raycast_pix_crwapper"); // TODO: rm
 
 // Handle game events
@@ -120,118 +117,4 @@ void wolfasm_events_mouse_motion_cwrapper(SDL_Event const *events) {
   int32_t mouse_x = events->motion.x;
   int32_t mouse_y = events->motion.y;
   (void)mouse_x, (void)mouse_y;
-}
-
-// C raycasting, for testing purpose
-// Taken from: http://lodev.org/cgtutor/raycasting.html
-void wolfasm_raycast_pix_crwapper(void) {
-  for (int32_t x = 0; x < MAP_WIDTH; ++x) {
-    double cameraX =
-        2.0 * x / (double)(MAP_WIDTH)-1.0; // x-coordinate in camera space
-    double rayPosX = game_player.pos_x;
-    double rayPosY = game_player.pos_y;
-    double rayDirX = game_player.dir_x + game_player.plane_x * cameraX;
-    double rayDirY = game_player.dir_y + game_player.plane_y * cameraX;
-
-    // which box of the map we're in
-    int mapX = (int)rayPosX;
-    int mapY = (int)rayPosY;
-
-    // length of ray from current position to next x or y-side
-    double sideDistX;
-    double sideDistY;
-
-    // length of ray from one x or y-side to next x or y-side
-    double deltaDistX = sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
-    double deltaDistY = sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
-    double perpWallDist;
-
-    // what direction to step in x or y-direction (either +1 or -1)
-    int stepX;
-    int stepY;
-
-    int hit = 0;  // was there a wall hit?
-    int side = 0; // was a NS or a EW wall hit?
-
-    // calculate step and initial sideDist
-    if (rayDirX < 0) {
-      stepX = -1;
-      sideDistX = (rayPosX - mapX) * deltaDistX;
-    } else {
-      stepX = 1;
-      sideDistX = (mapX + 1.0 - rayPosX) * deltaDistX;
-    }
-    if (rayDirY < 0) {
-      stepY = -1;
-      sideDistY = (rayPosY - mapY) * deltaDistY;
-    } else {
-      stepY = 1;
-      sideDistY = (mapY + 1.0 - rayPosY) * deltaDistY;
-    }
-
-    while (hit == 0) {
-      // jump to next map square, OR in x-direction, OR in y-direction
-      if (sideDistX < sideDistY) {
-        sideDistX += deltaDistX;
-        mapX += stepX;
-        side = 0;
-      } else {
-        sideDistY += deltaDistY;
-        mapY += stepY;
-        side = 1;
-      }
-      // Check if ray has hit a wall
-      if (map[mapY * map_width + mapX] > 0)
-        hit = 1;
-    }
-
-    // Calculate distance projected on camera direction (oblique distance will
-    // give fisheye effect!)
-    if (side == 0)
-      perpWallDist = (mapX - rayPosX + (1 - stepX) / 2) / rayDirX;
-    else
-      perpWallDist = (mapY - rayPosY + (1 - stepY) / 2) / rayDirY;
-
-    // Calculate height of line to draw on screen
-    int h = 480;
-    int lineHeight = (int)(h / perpWallDist);
-
-    // calculate lowest and highest pixel to fill in current stripe
-    int drawStart = -lineHeight / 2 + h / 2;
-    if (drawStart < 0)
-      drawStart = 0;
-    int drawEnd = lineHeight / 2 + h / 2;
-    if (drawEnd >= h)
-      drawEnd = h - 1;
-
-    // choose wall color
-    int32_t color;
-    assert(mapY * map_width + mapX < map_width * map_height);
-    switch (map[mapY * map_width + mapX]) {
-    case 1:
-      color = 0xC81543;
-      break; // red
-    case 2:
-      color = 0xA8E4B1;
-      break; // green
-    case 3:
-      color = 0x4AA3BA;
-      break; // blue
-    case 4:
-      color = 0xFFE9EC;
-      break; // white
-    default:
-      color = 0xF1E694;
-      break; // yellow
-    }
-
-    // give x and y sides different brightness
-    if (side == 1) {
-      color = color / 2;
-    }
-
-    for (int32_t i = drawStart; i < drawEnd; ++i) {
-      wolfasm_put_pixel(x, i, color);
-    }
-  }
 }
--- src/raycast.asm
+++ src/raycast.asm
@@ -7,7 +7,7 @@
         global wolfasm_raycast
 
         ;; wolfasm symbols
-        extern game_player
+        extern game_player, map, map_width, wolfasm_put_pixel
 
         ;; C function TODO: rm
         extern wolfasm_raycast_pix_crwapper
@@ -19,11 +19,6 @@
 wolfasm_raycast:
         push      rbp
         mov       rbp, rsp
-
-        call      wolfasm_raycast_pix_crwapper
-        mov       rsp, rbp
-        pop       rbp
-        ret
 
         xor       rdi, rdi
 .loop_x:
@@ -42,7 +37,6 @@ wolfasm_raycast:
         cvtsi2sd  xmm1, rax
         subsd     xmm0, xmm1
         movsd     [rel camera_x], xmm0
-
 
         ;; Initialize ray pos
         movsd     xmm0, [rel game_player + wolfasm_player.pos_x]
@@ -83,8 +77,12 @@ wolfasm_raycast:
         sub       rsp, 16
         movdqu    oword [rsp], xmm3 ;; save 1
         addsd     xmm0, xmm3
+        sub       rsp, 8
+        push      rdi               ;; Save counter
         call      _sqrt
-        movsd     [rel side_dist_x], xmm0
+        pop       rdi               ;; Restore counter
+        add       rsp, 8
+        movsd     [rel delta_dist_x], xmm0
         movdqu    xmm3, oword [rsp] ;; get -rotation_speed
         add       rsp, 16
         movdqu    xmm2, oword [rsp] ;; get -rotation_speed
@@ -95,26 +93,30 @@ wolfasm_raycast:
         movsd     xmm0, xmm2
         divsd     xmm0, xmm1
         addsd     xmm0, xmm3
+        sub       rsp, 8
+        push      rdi               ;; Save counter
         call      _sqrt
-        movsd     [rel side_dist_y], xmm0
+        pop       rdi               ;; Restore counter
+        add       rsp, 8
+        movsd     [rel delta_dist_y], xmm0
 
 .compute_steps:
-        xorpd     xmm0, xmm0
+        pxor      xmm0, xmm0
         movsd     xmm2, [rel delta_dist_x]
-        mov       rax, [rel map_x]
-        cvtsi2sd  xmm3, rax
+        mov       eax, [rel map_x]
+        cvtsi2sd  xmm3, eax
         movsd     xmm4, [rel ray_pos_x]
 .compute_step_x:
         movsd     xmm1, [rel ray_dir_x]
-        cmpsd     xmm0, xmm1, 2 ;; Lower or equal
-        ;; TODO: conditional jump ???
+        ucomisd   xmm0, xmm1
+        jbe       .compute_step_x_ge
         ;; xmm1 < 0
         mov       dword [rel step_x], -1
         subsd     xmm4, xmm3
         movsd     xmm1, xmm4
-
         jmp       .compute_step_x_end
-.compute_step_x_le:
+
+.compute_step_x_ge:
         ;; xmm1 >= 0
         mov       dword [rel step_x], 1
         subsd     xmm3, xmm4
@@ -123,26 +125,26 @@ wolfasm_raycast:
         addsd     xmm1, xmm3
 
 .compute_step_x_end:
-        mulsd     xmm0, xmm1
-        movsd     [rel side_dist_x], xmm0
+        mulsd     xmm2, xmm1
+        movsd     [rel side_dist_x], xmm2
 
 .compute_step_y:
-        xorpd     xmm0, xmm0
+        pxor      xmm0, xmm0
         movsd     xmm2, [rel delta_dist_y]
-        mov       rax, [rel map_y]
-        cvtsi2sd  xmm3, rax
+        mov       eax, [rel map_y]
+        cvtsi2sd  xmm3, eax
         movsd     xmm4, [rel ray_pos_y]
 
         movsd     xmm1, [rel ray_dir_y]
-        cmpsd     xmm0, xmm1, 2 ;; Lower or equal
-        ;; TODO: conditional jump ???
+        ucomisd   xmm0, xmm1
+        jbe       .compute_step_y_ge
         ;; xmm1 < 0
         mov       dword [rel step_y], -1
         subsd     xmm4, xmm3
         movsd     xmm1, xmm4
 
-        jmp       .compute_step_x_end
-.compute_step_y_le:
+        jmp       .compute_step_y_end
+.compute_step_y_ge:
         ;; xmm1 >= 0
         mov       dword [rel step_y], 1
         subsd     xmm3, xmm4
@@ -151,14 +153,218 @@ wolfasm_raycast:
         addsd     xmm1, xmm3
 
 .compute_step_y_end:
-        mulsd     xmm0, xmm1
+        mulsd     xmm2, xmm1
+        movsd     [rel side_dist_y], xmm2
+
+        ;; Compute hit distance
+        xor       rdx, rdx
+.hit_loop:
+        movsd     xmm0, [rel side_dist_x]
+        movsd     xmm1, [rel side_dist_y]
+        ucomisd   xmm1, xmm0
+        jbe       .hit_loop_y
+
+.hit_loop_x:
+        ;; Increment side distance
+        movsd     xmm0, [rel side_dist_x]
+        addsd     xmm0, [rel delta_dist_x]
+        movsd     [rel side_dist_x], xmm0
+
+        ;; Increment ray position
+        mov       eax, [rel step_x]
+        add       [rel map_x], eax
+
+        ;; dl is 'side'
+        mov       dl, 0
+
+        jmp       .hit_loop_check
+.hit_loop_y:
+        ;; Increment side distance
+        movsd     xmm0, [rel side_dist_y]
+        addsd     xmm0, [rel delta_dist_y]
         movsd     [rel side_dist_y], xmm0
 
-.hit_loop:
+        ;; Increment ray position
+        mov       eax, [rel step_y]
+        add       [rel map_y], eax
+
+        ;; dl is 'side'
+        mov       dl, 1
+
+.hit_loop_check:
+
+        ;; Check if ray hit a wall
+        ;; Get index
+        push      rdx
+        xor       rcx, rcx
+        mov       eax, [rel map_width]
+        mov       esi, [rel map_y]
+        mul       esi
+        mov       rcx, rax
+        add       cx, [rel map_x]
+        pop       rdx
+
+        ;; map[mapY * map_width + mapX] > 0
+        lea       rax, [rel map]
+        mov       byte cl, [rax + rcx]
+        cmp       byte cl, 0
+        ;; We hit a hall, stop iterating
+        jne       .compute_wall_distance
+
+        ;; Keep iterating
+        jmp       .hit_loop
+
+        ;; Calculate distance projected on camera direction
 .compute_wall_distance:
+        cmp       dl, 0
+        push      rdx                   ;; Save side for later
+        jne       .compute_wall_distance_y
+.compute_wall_distance_x:
+        mov       eax, [rel step_x]
+        mov       dword edx,  1
+        sub       edx, eax              ;; 1 - step_x
+        shr       edx, 1                ;; (1 - step_x) / 2
+        movsd     xmm0, [rel ray_pos_x]
+        cvtsi2sd  xmm1, [rel map_x]
+        subsd     xmm1, xmm0            ;; map_x - ray_pos_x
+        cvtsi2sd  xmm0, edx
+        addsd     xmm1, xmm0 ;; map_x - ray_pos_x + (1 - step_x) / 2
+        movsd     xmm0, [rel ray_dir_x]
+        divsd     xmm1, xmm0
+
+        jmp       .compute_line_height
+.compute_wall_distance_y:
+        mov       eax, [rel step_y]
+        mov       dword edx,  1
+        sub       edx, eax              ;; 1 - step_y
+        shr       edx, 1                ;; (1 - step_y) / 2
+        movsd     xmm0, [rel ray_pos_y]
+        cvtsi2sd  xmm1, [rel map_y]
+        subsd     xmm1, xmm0            ;; map_y - ray_pos_y
+        cvtsi2sd  xmm0, edx
+        addsd     xmm1, xmm0 ;; map_y - ray_pos_y + (1 - step_y) / 2
+        movsd     xmm0, [rel ray_dir_y]
+        divsd     xmm1, xmm0
+
+        ;; Calculate line to draw
+.compute_line_height:
+        mov       rax, WIN_HEIGHT
+        cvtsi2sd  xmm0, rax
+        divsd     xmm0, xmm1
+        ;; Convert result to integer
+        cvttsd2si esi, xmm0  ;; line_height is r8
+
+        ;; Compute line's limits
 .compute_limits:
+.compute_limits_start:
+        xor       r8d, r8d
+        mov       edx, WIN_HEIGHT / 2
+        mov       ecx, esi
+        shr       esi, 1      ;; line_height / 2
+        mov       ecx, esi
+        neg       ecx         ;; -line_height / 2
+        add       ecx, edx
+        cmp       ecx, 0
+        cmovl     ecx, r8d
+
+.compute_limits_end:
+        mov       r8d, WIN_HEIGHT - 1
+        mov       edx, WIN_HEIGHT / 2
+        add       esi, edx
+        cmp       esi, WIN_HEIGHT
+        cmovge    esi, r8d
+
+        ;; Determine wall color
+        ;; TODO: Find a cleaner / faster way to do it
 .compute_color:
+        ;; Get index
+        push      rcx   ;; Save draw_start
+        push      rsi   ;; Save draw_end
+        xor       rcx, rcx
+        mov       eax, [rel map_width]
+        mov       esi, [rel map_y]
+        mul       esi
+        mov       rcx, rax
+        add       cx, [rel map_x]
+
+        ;; map[mapY * map_width + mapX]
+        lea       rax, [rel map]
+        mov       byte dl, [rax + rcx]
+
+.compute_color_red:
+        cmp       byte dl, 1
+        jne       .compute_color_green
+        mov       eax, 0xC81543 ;; Red color
+        jmp       .check_shadow_color
+
+.compute_color_green:
+        cmp       byte dl, 2
+        jne       .compute_color_blue
+        mov       eax, 0xA8E4B1 ;; Green color
+        jmp       .check_shadow_color
+
+.compute_color_blue:
+        cmp       byte dl, 3
+        jne       .compute_color_white
+        mov       eax, 0x4AA3BA ;; Blue color
+        jmp       .check_shadow_color
+
+.compute_color_white:
+        cmp       byte dl, 4
+        jne       .compute_color_default
+        mov       eax, 0xFFE9EC ;; White color
+        jmp       .check_shadow_color
+
+.compute_color_default:
+        mov       eax, 0xF1E694 ;; Yellow color
+
+        ;; Simulate shadow
+.check_shadow_color:
+        pop       rsi      ;; Restore draw_end
+        pop       rcx;     ;; Restore draw_start
+        pop       rdx      ;; Get back 'side'
+        cmp       byte dl, 1
+        jne       .draw_scene_pre
+        shr       eax, 1    ;; color / 2
+
+        ;; TODO: Optimize
+        ;; Draw line to the screen
+.draw_scene_pre:
+        ;; Swap draw_start and draw_end for conveniance
+        ;; (simpler to call wolfasm_put_pixel)
+        mov       rdx, rsi
+        mov       rsi, rcx
+        mov       rcx, rdx
+
+        ;; Same that comment above
+        mov       edx, eax
+
 .draw_scene:
+        cmp       rsi, rcx
+        je        .draw_scene_end
+
+        sub       rsp, 8
+        push      rsi     ;; Save current_state
+        sub       rsp, 8
+        push      rcx     ;; Save draw_end
+        sub       rsp, 8
+        push      rdi     ;; Save loop counter
+        sub       rsp, 8
+        push      rdx     ;; Save color
+        call      wolfasm_put_pixel
+        pop       rdx
+        add       rsp, 8
+        pop       rdi
+        add       rsp, 8
+        pop       rcx
+        add       rsp, 8
+        pop       rsi
+        add       rsp, 8
+
+        inc       rsi
+        jmp       .draw_scene
+
+.draw_scene_end:
 
         inc       rdi
         jmp       .loop_x
