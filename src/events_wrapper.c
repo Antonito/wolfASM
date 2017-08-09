8b615023fe1f5f5776c500f22e691dc40f2b3352
Antoine Bache <antoine.bache@epitech.eu>
2017-08-09 04:21:33 +0200

    Removed a TODO

--- src/events_wrapper.c
+++ src/events_wrapper.c
@@ -84,8 +84,7 @@ void wolfasm_events_mouse_down_cwrapper(SDL_Event const *events) {
   switch (events->button.button) {
   case SDL_BUTTON_LEFT:
     if (game_player.weapon->sprite->trigger == 0) {
-      wolfasm_play_sound(
-          game_player.weapon->sound); // TODO: change to play weapon's sound
+      wolfasm_play_sound(game_player.weapon->sound);
       game_player.weapon->sprite->trigger = 1;
     }
     break;
